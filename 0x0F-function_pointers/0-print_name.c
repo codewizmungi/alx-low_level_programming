ad654104289d19a6571d0a3ed16dfd1a6c60c515
Mungwa Agu <mungwaagu@gmail.com>
2023-06-21 10:20:19 -0700

    Added function_pointers.h and 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - A function that prints a name
+ * @name: name to be printed
+ * @f: Function Pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		(*f)(name);
+	}
+}
