f76c69f8a37c2471cfee69b93036e61900684dbd
Mungwa Agu <mungwaagu@gmail.com>
2023-07-03 13:14:27 -0700

    Added 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,27 +12,29 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 
 	list_t *current_node, *new_node;
-	int a;
+	int a = 0;
+
+	while (str[a] != '\0')
+		a++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
-
-	for (a = 0; str[a]; a++)
-		;
-
-	new_node->len = a;
 	new_node->next = NULL;
+	new_node->len = a;
+
 	current_node = *head;
 
 	if (current_node == NULL)
 		*head = new_node;
 	else
+	{
+		while (current_node->next != NULL)
+			current_node = current_node->next;
 		current_node->next = new_node;
-
+	}
 
 	return (new_node);
-
 }
