0cf115b003a803398ed68e5bc694d18e2dfeb75c
Mungwa Agu <mungwaagu@gmail.com>
2023-07-10 16:14:29 -0700

    Added 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1);
 
-	if(text_content)
+	if (text_content)
 	{
 		while (text_content[char_length])
 			char_length++;
